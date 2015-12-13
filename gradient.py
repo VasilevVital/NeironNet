d63c3f62cc0766461277e9147280d9dd47f20d06
Mihayl <druidich92@yandex.ru>
2015-12-13 18:15:13 +0500

    небольшие нейросети. заготовки add gradient descent

---
+++ gradient.py
@@ -0,0 +1,19 @@
+import numpy as np
+#градиентный спуск
+X = np.array([[0, 0, 1],
+              [0, 1, 1],
+              [1, 0, 1],
+              [1, 1, 1]])
+y = np.array([0, 1, 1, 0]).T
+
+alpha, hidden_dim = (0.5, 4)
+
+synaplse_0 = 2 * np.random.random((3, hidden_dim)) - 1
+synaplse_1 = 2 * np.random.random((hidden_dim, 1)) - 1
+for j in range(60000):
+    layer_1 = 1 / (1 + np.exp(-(np.dot(X, synaplse_0))))
+    layer_2 = 1 / (1 + np.exp(-(np.dot(layer_1, synaplse_1))))
+    layer_2_delta = (layer_2 - y) * (layer_2 * (1 - layer_2))
+    layer_1_delta = layer_2_delta.dot(synaplse_1.T) * (layer_1 * (1 - layer_1))
+    synaplse_1 -= (alpha * layer_1.T.dot(layer_2_delta))
+    synaplse_0 -= (alpha * X.T.dot(layer_1_delta))
